2bf169fb1a77b945fb253ea23fdf91444bae1579
BW05GuoJianChao <2605579851@qq.com>
2019-09-05 20:05:25 +0800

    先提交

---
+++ src/main/java/com/bw/entity/Demo.java
@@ -0,0 +1,15 @@
+package com.bw.entity;
+/**
+ * @作者 国建超
+ * @日期 2019年9月5日
+ * @时间 下午8:04:28
+ * 类功能：
+ *
+ */
+public class Demo {
+     public static void main(String[] args) {
+		
+    	 System.out.println("hellow");
+    	 
+	}
+}
